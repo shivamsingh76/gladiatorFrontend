2d2f1730d0b3340531e3d794586f69292f90758a
Sameerrana <cricketer.sam14@gmail.com>
2022-11-30 14:57:01 +0530

    Login Register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserLoginComponent } from './Login/user-login/user-login.component';
+import { UserRegisterComponent } from './Register/user-register/user-register.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserLoginComponent,
+    UserRegisterComponent,
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule, ReactiveFormsModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
