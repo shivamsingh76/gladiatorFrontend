c472a99b604f0fb699911a5cb90d5ee9448baff2
amarnath <amarnathdp17@gmail.com>
2022-11-30 14:57:16 +0530

    category frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShowAllCategoryComponent } from './Category/show-all-category/show-all-category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShowAllCategoryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
