e5e146cc405c64729a7842f7851f0308bd7f85fd
Sameerrana <cricketer.sam14@gmail.com>
2022-11-30 15:47:04 +0530

    login

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,27 +1,24 @@
 import { Injectable } from '@angular/core';
-import { HttpClient,HttpParams  } from '@angular/common/http';
-import { Login } from '../Login/login';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
 import { User } from '../User/User';
 import { Observable } from 'rxjs';
 import { register } from '../Register/register';
+import { Login } from '../Login/Login';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  baseUrl:string="http://localhost:8282/users";
-  constructor(private httpService:HttpClient) { }
-
-
-  public doAdminLogin(login:Login):Observable<any> {
-    return this.httpService.get<User>(this.baseUrl+'/login/'+login.email+"/"+login.password);
+  baseUrl: string = "http://localhost:8282/users";
+  constructor(private httpService: HttpClient) { }
+  public doAdminLogin(login: Login): Observable<any> {
+    return this.httpService.get<User>(this.baseUrl + '/login/' + login.email + "/" + login.password);
   }
 
-  public addProd(emp:register):Observable<any>
-  {
-    const headers = {'content-type': 'application/json'}
-    console.log("inside service"+emp);
-    return this.httpService.post(this.baseUrl+'/adduser/',JSON.stringify(emp),{'headers':headers});
+  public addProd(emp: register): Observable<any> {
+    const headers = { 'content-type': 'application/json' }
+    console.log("inside service" + emp);
+    return this.httpService.post(this.baseUrl + '/products/', JSON.stringify(emp), { 'headers': headers });
   }
-  //http://localhost:8282/users/adduser
 }
